d142b8b16d9689c22e2a46531ac3ec9569912296
Alan Reed <alan_p_reed@hotmail.co.uk>
2019-08-26 21:51:49 +0100

    Moved axes, fixed polygon and edge drawing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import planck from 'planck-js'
 
-// let cv = document.getElementById('js13k_canvas');
-// let ctx = cv.getContext('2d');
-// ctx.scale(4,4)
+let cv = document.getElementById('js13k_canvas');
+let ctx = cv.getContext('2d');
+
+// Flip y axis, move 0,0 to centre bottom, increase size
+ctx.translate(cv.width/2, cv.height);
+ctx.scale(4,-4);
 
 var world = planck.World({
   gravity: planck.Vec2(0, -10)
@@ -21,7 +24,7 @@ var shape = planck.Box(0.6, 0.125);
 
 var y = 25.0;
 var prevBody = ground;
-for (let i = 0; i < 3; ++i) {
+for (let i = 0; i < 30; ++i) {
   var body = world.createDynamicBody(planck.Vec2(0.5 + i, y));
   body.createFixture(shape, {
     density: 20.0,
@@ -40,26 +43,19 @@ for (let i = 0; i < 3; ++i) {
 function drawEdge(ctx, edge, position, angle, linewidth, strokeStyle) {
   let v1 = edge.m_shape.m_vertex1;
   let v2 = edge.m_shape.m_vertex2;
-3
-  let dx = v2.x - v1.x;
-  let dy = v2.y - v1.y;
-
-  let length = Math.sqrt(dx * dx + dy * dy);
 
   ctx.save();
   ctx.translate(position.x, position.y);
   ctx.rotate(angle);
-  ctx.scale(4,4);
 
   ctx.beginPath();
-  ctx.moveTo(linewidth, linewidth);
-  ctx.lineTo(linewidth + length, linewidth);
+  ctx.moveTo(v1.x, v1.y);
+  ctx.lineTo(v2.x, v2.y);
 
   ctx.lineCap = 'round';
   ctx.lineWidth = linewidth;
   ctx.strokeStyle =  strokeStyle;
   ctx.stroke();
-
   ctx.restore();
 };
 
@@ -70,42 +66,22 @@ function drawPolygon(ctx, shape, position, angle, linewidth, strokeStyle, scaleY
     return;
   }
 
-  let minX = Infinity, minY = Infinity;
-  let maxX = -Infinity, maxY = -Infinity;
-
-  for (let i = 0; i < vertices.length; ++i) {
-    let v = vertices[i];
-    minX = Math.min(minX, v.x);
-    maxX = Math.max(maxX, v.x);
-    minY = Math.min(minY, scaleY * v.y);
-    maxY = Math.max(maxY, scaleY * v.y);
-  }
-
   ctx.save();
   ctx.translate(position.x, position.y);
   ctx.rotate(angle);
-  ctx.scale(4,4);
 
-  ctx.beginPath();
   for (let i = 0; i < vertices.length; ++i) {
     let v = vertices[i];
-    let x = v.x - minX + linewidth;
-    let y = scaleY * v.y - minY + linewidth;
     if (i == 0)
-      ctx.moveTo(x, y);
+      ctx.moveTo(v.x, v.y);
     else
-      ctx.lineTo(x, y);
-  }
-
-  if (vertices.length > 2) {
-    ctx.closePath();
+      ctx.lineTo(v.x, v.y);
   }
 
   ctx.lineCap = 'round';
   ctx.lineWidth = linewidth;
   ctx.strokeStyle = strokeStyle;
   ctx.stroke();
-
   ctx.restore();
 };
 
@@ -115,22 +91,19 @@ function step() {
 
   let canvas = document.getElementById('js13k_canvas');
   let context = canvas.getContext('2d');
-  context.save();
-
+  
   // Use the identity matrix while clearing the canvas
+  context.save();
   context.setTransform(1, 0, 0, 1, 0, 0);
   context.clearRect(0, 0, canvas.width, canvas.height);
-  
-  // Restore the transform
   context.restore();
 
   for (let body = world.getBodyList(); body; body = body.getNext()) {
     for (let fixture = body.getFixtureList(); fixture; fixture = fixture.getNext()) {
-      // draw or update fixture
-      
+
       let type = fixture.getType();
       let shape = fixture.getShape();
-      let position = body.getPosition();
+      let position = body.getWorldCenter();
       let rotation = body.getAngle();
 
       if (type == 'circle') {
@@ -140,7 +113,7 @@ function step() {
         drawEdge(context, fixture, position, rotation, 2, "#FF0000");
       }
       if (type == 'polygon') {
-        drawPolygon(context, shape, position, rotation, 2, "#000000", -1);
+        drawPolygon(context, shape, position, rotation, 2, "#000000",  1);
       }
       if (type == 'chain') {
         //f.ui = viewer.drawChain(shape, options);
